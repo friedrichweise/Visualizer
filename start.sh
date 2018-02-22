f0bca882a2bec9daefb0186daff5022d132e7cec
friedrichweise <account@weise.io>
2018-02-22 20:52:46 +0100

    remove uncommented run command

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-#processing-java --force --sketch=$PWD --build=/Users/friedrichweise/repos/visualizer/ --present  --run
 processing-java --sketch=$(cd "$(dirname "$0")" && pwd) --output=$(cd "$(dirname "$0")" && pwd)/output --force --run
